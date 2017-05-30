95babeb2e159c0f19e55c8df0967c01eb4997073
Mark Larah <mark@larah.me>
2017-05-30 04:26:56 -0700

    fixes to package build

--- packages/composerize/rollup.config.js
+++ packages/composerize/rollup.config.js
@@ -2,11 +2,17 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace'
 
 export default {
   entry: 'src/index.js',
   format: 'cjs',
+  sourceMap: 'inline',
   plugins: [
+    replace({
+       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     resolve(),
     babel({
       exclude: 'node_modules/**', // only transpile our source code
@@ -14,6 +20,7 @@ export default {
     commonjs({
       namedExports: { 'node_modules/spawn-args/index.js': ['spawnargs'] }, // Default: undefined
     }),
+    uglify()
   ],
   dest: 'dist/bundle.js',
 };
