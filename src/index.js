deaff02b7aa28ddc652a4210031613f569bb1cba
Mark Larah <mark@larah.me>
2017-11-05 06:21:47 -0800

    Add/Update eslint related deps
    - @mlarah/eslint-config => @sharkcore/eslint-config
    - add pre-commit safety with husky
    - bump eslint, babel deps

--- src/index.js
+++ src/index.js
@@ -1,52 +1,52 @@
 // @flow
-import yamljs from "yamljs";
-import parser from "yargs-parser";
-import deepmerge from "deepmerge";
+import yamljs from 'yamljs';
+import parser from 'yargs-parser';
+import deepmerge from 'deepmerge';
 
-import { maybeGetComposeEntry, getComposeJson } from "./logic";
+import { maybeGetComposeEntry, getComposeJson } from './logic';
 
 const getServiceName = image => {
-  let name = image.includes("/") ? image.split("/")[1] : image;
-  name = name.includes(":") ? name.split(":")[0] : name;
+    let name = image.includes('/') ? image.split('/')[1] : image;
+    name = name.includes(':') ? name.split(':')[0] : name;
 
-  return name;
+    return name;
 };
 
 export default (input: string): ?string => {
-  const formattedInput = input.replace(/(\s)+/g, " ").trim();
-  const tokens = parser(formattedInput);
+    const formattedInput = input.replace(/(\s)+/g, ' ').trim();
+    const tokens = parser(formattedInput);
 
-  if (tokens._[0] !== "docker" || tokens._[1] !== "run") {
-    throw new SyntaxError("must be a valid docker run command");
-  }
+    if (tokens._[0] !== 'docker' || tokens._[1] !== 'run') {
+        throw new SyntaxError('must be a valid docker run command');
+    }
 
-  // The service object that we'll update
-  let service = {};
+    // The service object that we'll update
+    let service = {};
 
-  // Loop through the tokens and append to the service object
-  Object.entries(tokens)
-    .filter(t => t[0] !== "_")
-    .forEach(([key, value]) => {
-      const composeEntry = maybeGetComposeEntry(key, value);
-      if (composeEntry) {
-        // Store whatever the next entry will be
-        const json = getComposeJson(composeEntry);
-        service = deepmerge(service, json);
-      }
-    });
+    // Loop through the tokens and append to the service object
+    Object.entries(tokens)
+        .filter(t => t[0] !== '_')
+        .forEach(([key, value]) => {
+            const composeEntry = maybeGetComposeEntry(key, value);
+            if (composeEntry) {
+                // Store whatever the next entry will be
+                const json = getComposeJson(composeEntry);
+                service = deepmerge(service, json);
+            }
+        });
 
-  const image = tokens._[tokens._.length - 1];
-  service.image = image;
+    const image = tokens._[tokens._.length - 1];
+    service.image = image;
 
-  let serviceName = getServiceName(image);
+    const serviceName = getServiceName(image);
 
-  // Outer template
-  const result = {
-    version: "3.3",
-    services: {
-      [serviceName]: service,
-    },
-  };
+    // Outer template
+    const result = {
+        version: '3.3',
+        services: {
+            [serviceName]: service,
+        },
+    };
 
-  return yamljs.stringify(result, 9, 4).trim();
+    return yamljs.stringify(result, 9, 4).trim();
 };
--- src/logic.js
+++ src/logic.js
@@ -1,85 +1,85 @@
 // @flow
 
-type RawValue = string | number | boolean | [string | number | boolean];
-
 import type {
-  ComposeEntry,
-  KVComposeEntry,
-  ArrayComposeEntry,
-  SwitchComposeEntry,
-  ValueComposeEntry,
-  Mapping,
-} from "./mappings";
-import { MAPPINGS } from "./mappings";
+    ComposeEntry,
+    KVComposeEntry,
+    ArrayComposeEntry,
+    SwitchComposeEntry,
+    ValueComposeEntry,
+    Mapping,
+} from './mappings';
+import { MAPPINGS } from './mappings';
+
+type RawValue = string | number | boolean | [string | number | boolean];
 
 /**
  * Turn a mapping and the value of the mapping into a formatted json object
  */
 export const getComposeEntry = (
-  mapping: Mapping,
-  value: RawValue,
+    mapping: Mapping,
+    value: RawValue,
 ): ComposeEntry => {
-  if (mapping.type === "KeyValue") {
-    return ({
-      path: mapping.path,
-      value: {
-        [value.split("=")[0]]: value.split("=")[1],
-      },
-    }: KVComposeEntry);
-  }
+    if (mapping.type === 'KeyValue') {
+        return ({
+            path: mapping.path,
+            value: {
+                [value.split('=')[0]]: value.split('=')[1],
+            },
+        }: KVComposeEntry);
+    }
 
-  if (mapping.type === "Array") {
-    return ({
-      path: mapping.path,
-      value: Array.isArray(value) ? value : [value],
-    }: ArrayComposeEntry);
-  }
+    if (mapping.type === 'Array') {
+        return ({
+            path: mapping.path,
+            value: Array.isArray(value) ? value : [value],
+        }: ArrayComposeEntry);
+    }
 
-  if (mapping.type === "Switch") {
-    return ({
-      path: mapping.path,
-      value: value === "true",
-    }: SwitchComposeEntry);
-  }
+    if (mapping.type === 'Switch') {
+        return ({
+            path: mapping.path,
+            value: value === 'true',
+        }: SwitchComposeEntry);
+    }
 
-  return ({
-    path: mapping.path,
-    value,
-  }: ValueComposeEntry);
+    return ({
+        path: mapping.path,
+        value,
+    }: ValueComposeEntry);
 };
 
 /**
  * Map a token(s) to the argument type and value
  */
 export const maybeGetComposeEntry = (
-  /* The docker cli argument key (what to look up in the mapping objects) */
-  mapKey: string,
-  /* The value(s) to be applied */
-  value: RawValue,
+    /* The docker cli argument key (what to look up in the mapping objects) */
+    mapKey: string,
+    /* The value(s) to be applied */
+    value: RawValue,
 ): ?ComposeEntry => {
-  // The 'Mapping' object (to map from the cli key to the docker compose equivalent structure)
-  const mapping = MAPPINGS[mapKey];
+    // The 'Mapping' object (to map from the cli key to the docker compose equivalent structure)
+    const mapping = MAPPINGS[mapKey];
 
-  if (!mapping) {
+    if (!mapping) {
+        // TODO: Throw error / warning
+        return null;
+    }
+
+    // Ensure there is a value
+    if (mapping.type !== 'Switch' && !value) {
+        // TODO: Throw error / warning
+        return null;
+    }
+
+    if (mapping && value) {
+        return getComposeEntry(mapping, value);
+    }
+
     // TODO: Throw error / warning
     return null;
-  }
-
-  // Ensure there is a value
-  if (mapping.type !== "Switch" && !value) {
-    // TODO: Throw error / warning
-    return null;
-  }
-
-  if (mapping && value) {
-    return getComposeEntry(mapping, value);
-  }
-
-  // TODO: Throw error / warning
-  return null;
 };
 
 export const getComposeJson = (entry: ComposeEntry): any =>
-  entry.path
-    .split("/")
-    .reduceRight((prev, pathItem) => ({ [pathItem]: prev }), entry.value);
+    entry.path
+        .split('/')
+        .reduceRight((prev, pathItem) => ({ [pathItem]: prev }), entry.value);
