e8c94fc8fcef59552dc192261b5f6b38ee354b45
Mark Larah <mark@larah.me>
2018-03-04 18:42:00 -0800

    fix build

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable */
 
 const composerize = require('./dist/composerize');
 
 const command = process.argv.slice(2).join(' ');
-// eslint-disable-next-line no-console
 console.log(composerize(command));
