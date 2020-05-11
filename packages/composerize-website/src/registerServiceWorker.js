162ecf600e30e5834eb484a37ab31004ef176aa7
Mark Larah <mark@larah.me>
2020-05-10 17:32:03 -0700

    add husky

--- packages/composerize-website/src/registerServiceWorker.js
+++ packages/composerize-website/src/registerServiceWorker.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
 export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
-    });
-  }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then(registration => {
+            registration.unregister();
+        });
+    }
 }
