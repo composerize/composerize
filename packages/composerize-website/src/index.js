3913de86cfc22495ed899fb53da0bdba92ef9745
Mark Larah <mark@larah.me>
2020-05-10 17:18:07 -0700

    try busting the service worker cache

--- packages/composerize-website/src/index.js
+++ packages/composerize-website/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// don't bother caching...
+unregister();
