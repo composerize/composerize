43332f36b360e75b10a95ab99d0c34306984dc61
sharevb <sharevb@gmail.com>
2023-11-12 23:50:41 +0100

    Allow composerize cli to take an existing docker compose in stdin

--- packages/composerize/cli.js
+++ packages/composerize/cli.js
@@ -4,4 +4,15 @@
 const composerize = require('./dist/composerize');
 
 const command = process.argv.slice(2).join(' ');
-console.log(composerize(command));
+
+if (process.stdin.isTTY){
+	console.log(composerize(command));
+}
+else {
+	var existingDockerCompose = '';
+	process.stdin.on('data', function(d) {
+		existingDockerCompose += d;
+	}).on('end', function() {
+		console.log(composerize(command, existingDockerCompose));
+	}).setEncoding('utf8');
+}
